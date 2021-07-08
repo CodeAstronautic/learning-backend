67bba2df59c86d4b548253b885591486c2231ecb
Srijan Sil <subratasil2016@gmail.com>
2021-07-08 21:36:33 +0530

    added jwt

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 /*const User = require("../models/User")*/
 const bcrypt = require('bcrypt')
-
+const jwt = require('jsonwebtoken')
 
 router.post("/login", async (req, res) =>{
     try {

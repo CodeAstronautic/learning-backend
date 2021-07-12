297c0e33b716d1366b569983572a614f493adf2e
Srijan Sil <subratasil2016@gmail.com>
2021-07-13 01:18:30 +0530

    added jwt verification middleware to post routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const postController = require('../controllers/postController');
+const verify = require('./verifyToken');
 
-router.post('/create', postController.createPost);
-router.get('/get', postController.getPost);
-router.put('/update/:id', postController.updatePost);
-router.delete('/delete/:id', postController.deletePost);
-router.put('/tag/:id', postController.tagPosts);
-router.post('/comment/:id', postController.postComment);
-router.get('/get/comments/:id', postController.getComment);
+router.post('/create',verify, postController.createPost);
+router.get('/get',verify, postController.getPost);
+router.put('/update/:id',verify, postController.updatePost);
+router.delete('/delete/:id',verify, postController.deletePost);
+router.put('/tag/:id',verify, postController.tagPosts);
+router.post('/comment/:id',verify, postController.postComment);
+router.get('/get/comments/:id',verify, postController.getComment);
 
 module.exports = router;
